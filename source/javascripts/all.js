e78dac503b0fa030918e3e9ed6cf49e7387fe597
Sibin Xavier <sibin@redpanthers.co>
2016-01-21 10:02:13 +0530

    Changes, add github js and update templates

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,28 +1,19 @@
 //= require jquery-1.11.3.min
 //= require bootstrap.min
+//= require github
 //= require isotope.pkgd.min
 //= require angular.min
 //= require angular-animate.min
 //= require angular-ui-router.min
-//= require ui-bootstrap-tpls-1.0.3.min
-
-//= require github
-//= require angular.min
-//= require angular-animate.min
-//= require angular-ui-router.min
-
 //= require angular-isotope.min
 //= require angular-smooth-scroll.min
 //= require angular-loading-bar.min
 //= require angular-site
-
 //= require filter/string-filter
 
-//= require service/openSourceService
+//= require service/github-api.js
 //= require controller/homeController
 //= require controller/headerController
 //= require controller/categoryController
-
 //= require controller/teamController
-
 //= require directive/commonBootstrapDirectives 
--- source/javascripts/controller/homeController.js
+++ source/javascripts/controller/homeController.js
@@ -1,6 +1,14 @@
-var RedPanther = window.RedPanther || angular.module('redpanther',[])
-RedPanther.controller('homeController',['$scope','openSouceProjects',function($scope,openSouceProjects){
+var RedPanther = window.RedPanther || angular.module('redpanther',[]);
+RedPanther.controller('homeController',['$scope','githubApis',function($scope,githubApis){
   $scope.title = "Redpanther IO";
-  $scope.getCategoryList = openSouceProjects.getProjectCategories();
-  $scope.projects = openSouceProjects.getTenProjectsByCategory();
+  $scope.getCategoryList = []
+  githubApis.getAllLanguages().then(function(values){
+  	$scope.getCategoryList =  values
+	});
+
+	$scope.getProjects = [] 
+	githubApis.getAllRepository().then(function(values){
+		$scope.getProjects = values
+	})
+  
 }])
