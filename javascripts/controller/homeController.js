8866de475ec797f71e8c4a894fab6ae4696a186b
Sibin Xavier <sibin@redpanthers.co>
2016-01-27 15:28:36 +0530

    Remove unwated files

--- javascripts/controller/homeController.js
+++
@@ -1,7 +0,0 @@
-var RedPanther = window.RedPanther || angular.module('redpanther',[])
-RedPanther.controller('homeController',['$scope','openSouceProjects',function($scope,openSouceProjects){
-  $scope.title = "Redpanther IO";
-  $scope.getCategoryList = openSouceProjects.getProjectCategories();
-  $scope.projects = openSouceProjects.getTenProjectsByCategory();
-}])
-;
