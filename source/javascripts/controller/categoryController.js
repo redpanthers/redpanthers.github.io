01f8f233e83f6b91bfc0d819897a7742662bba69
Sibin Xavier <sibin@redpanthers.co>
2016-01-27 15:48:07 +0530

    New header background, change redpanther to redpanthers in module name

--- source/javascripts/controller/categoryController.js
+++ source/javascripts/controller/categoryController.js
@@ -1,8 +1,8 @@
 var RedPanthers = window.RedPanthers || angular.module('redpanthers',[])
 RedPanthers.controller('categoryController',['$scope','$state','$stateParams','openSouceProjects',function($scope,$state,$stateParams,openSouceProjects){
   $scope.categories = openSouceProjects.getProjectCategories();
-  var categorySlug = $stateParams.categorySlug;  
+  var categorySlug = $stateParams.categorySlug;
   $scope.category = openSouceProjects.getCategoryDetailBySlug(categorySlug)
-  
+
   $scope.projectByCategory = openSouceProjects.getProjectsByCategorySlug(categorySlug)
 }])
