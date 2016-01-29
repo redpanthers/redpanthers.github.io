d175f4e82b04c2c7a4868817de4cb65e3b6a19bc
Sibin Xavier <sibin@redpanthers.co>
2016-01-29 09:40:24 +0530

    Contribution section updated, team members shown in different way

--- source/javascripts/controller/contributionController.js
+++ source/javascripts/controller/contributionController.js
@@ -1,15 +1,18 @@
-var RedPanthers = window.RedPanthers || angular.module('redpanthers',{});
-RedPanthers.controller('contributionController',['$scope','$resource','rubygemApi',function($scope,$resource,rubygemApi){
-  var contributors = ["coderhs",'manusajith']
+var RedPanthers = window.RedPanthers || angular.module('redpanthers', {});
+RedPanthers.controller('contributionController', ['$scope', '$resource','githubApis', 'rubygemApi', function($scope, $resource, githubApis,rubygemApi) {
+  var contributors = ["coderhs", 'manusajith']
   var rubyGemArray = [];
   $scope.rubyGemArray = []
-  contributors.map(function(user,index){
-    rubygemApi.getEachUsersRepo(user).then(function(result){
+  contributors.map(function(user, index) {
+    rubygemApi.getEachUsersRepo(user).then(function(result) {
 
-      rubyGemArray = _.unionBy(rubyGemArray, result,'name')
+      rubyGemArray = _.unionBy(rubyGemArray, result, 'name')
       $scope.rubyGemArray = rubyGemArray
-    })    
+    })
   })
-  
-  
+  $scope.getProjects = []
+  githubApis.getAllRepository().query(function(values) {
+    $scope.getProjects = values
+  })
+
 }])
