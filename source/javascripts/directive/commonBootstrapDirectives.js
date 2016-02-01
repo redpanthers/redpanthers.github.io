990f921ffff192fd15941de766fcb356950b3e79
Sibin Xavier <sibin@redpanthers.co>
2016-02-01 09:39:07 +0530

    Add new home page

--- source/javascripts/directive/commonBootstrapDirectives.js
+++ source/javascripts/directive/commonBootstrapDirectives.js
@@ -10,3 +10,19 @@ RedPanthers.directive('imageOnLoad',function(){
   }
   return imageOn;
 })
+RedPanthers.directive('fixedHomeSection',function(){
+  var homeSection = {};
+  homeSection.restrict = 'A';
+  homeSection.link = function(scope,element,attr){
+     
+    $(window).scroll(function(){
+      var requiredHeight = $('.project-list-nav').height();
+      var requiredTop = $(window).scrollTop();
+      $(element).css({height:requiredHeight})
+      $('.inner-home-about').css({
+        top: requiredTop
+      })
+    })
+  }
+  return homeSection;
+})
---
+++ source/javascripts/main.js
@@ -0,0 +1,5 @@
+$(window).load(function(){
+  if($('.category-items').length > 0){
+    $('.category-items').children().first().find('a').click();    
+  }
+})
